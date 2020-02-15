f9dd769cf2fb385bcef15771b53fa3164e6354f8
veselkovmatvei <60728971+veselkovmatvei@users.noreply.github.com>
2020-02-15 09:46:47 +0300

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -5,7 +5,8 @@ from pyrob.api import *
 
 @task
 def task_1_1():
-    pass
+    move_right(2)
+    move_down(1)
 
 
 if __name__ == '__main__':
--- task_2.py
+++ task_2.py
@@ -4,9 +4,12 @@ from pyrob.api import *
 
 
 @task
-def task_1_2():
-    pass
-
+def task_1_2():   
+    move_right(2)
+    move_down(2)
+    fill_cell()
+    move_right(2)
+    move_down(1)
 
 if __name__ == '__main__':
     run_tasks()
