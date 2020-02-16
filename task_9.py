443144c63605293ee3047ae15c3df496ac5f1ae5
veselkovmatvei <60728971+veselkovmatvei@users.noreply.github.com>
2020-02-16 04:16:17 +0300

    Add files via upload

--- task_9.py
+++ task_9.py
@@ -5,8 +5,14 @@ from pyrob.api import *
 
 @task
 def task_8_2():
-    pass
-
+     for i in range(30):
+         if not wall_is_above() and not wall_is_on_the_right():
+                fill_cell()
+                move_right(1)
+         elif not wall_is_above() and wall_is_on_the_right():
+                  fill_cell() 
+         elif wall_is_above() and not wall_is_on_the_right():
+              move_right(1)
 
 if __name__ == '__main__':
     run_tasks()
