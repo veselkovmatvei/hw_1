0e7da24908cf2d639db9539ec8dd22c53da1e24a
veselkovmatvei <60728971+veselkovmatvei@users.noreply.github.com>
2020-03-09 12:30:52 +0300

    Add files via upload

--- task_7.py
+++ task_7.py
@@ -5,8 +5,23 @@ from pyrob.api import *
 
 @task
 def task_5_4():
-    pass
+      
+      for i in range(20):
+          if not wall_is_beneath():
+                 move_down()
+      for i in range(20):
+          if wall_is_beneath():
+             move_right()
 
+      for i in range(1):
+          move_down()
+          move_left()
+
+      for i in range(20):
+          if not wall_is_on_the_left():
+                 move_left()
+      
 
 if __name__ == '__main__':
     run_tasks()
+    
--- task_8.py
+++ task_8.py
@@ -5,8 +5,11 @@ from pyrob.api import *
 
 @task
 def task_5_7():
-    for i in range(20):
-        if wall_is_beneath or wall_is_above():
-            move_right()
+        for i in range(20):
+            if wall_is_above():
+               move_right()
+            elif wall_is_beneath():
+                move_right()
+
 if __name__ == '__main__':
     run_tasks()
